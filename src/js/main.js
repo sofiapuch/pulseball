4f397b3515d002f213a3c01880d5b0e53b7e96a8
Sofia Puch <sophiepuch@gmail.com>
2016-05-26 12:35:16 +0100

    added json with initial rankings, read the json and display the result

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
-console.log("testing everything works");
+var pulseballApp = angular.module('pulseballApp', []);
+
+pulseballApp.controller('pulseballAppCtrl', ['$scope', '$http', function( $scope, $http) {
+	
+
+	//Pulseball init
+	$scope.pulseballInit = function (rankingsJson) {
+
+		console.log(rankingsJson);
+
+		var successCallback = $scope.pulseballInitSuccess;
+		var errorCallback = $scope.pulseballInitError;
+
+		$http.get(rankingsJson).success(successCallback).error(errorCallback);
+
+	};
+
+	$scope.pulseballInitSuccess = function (response) {
+		console.log("data: ", response);
+		$scope.ranking = response;
+	};
+
+	$scope.pulseballInitError = function (response) {
+		console.log("error loading ", rankingsJson);
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+}]);
+
