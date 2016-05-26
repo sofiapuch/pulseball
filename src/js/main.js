13ba44338e2e5035682cfb43efed74c49347e74c
Sofia Puch <sophiepuch@gmail.com>
2016-05-26 18:23:07 +0100

    added new match as json file (understanding there's only one match each time), calculate the rating difference between the two teams

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,90 @@ var pulseballApp = angular.module('pulseballApp', []);
 
 pulseballApp.controller('pulseballAppCtrl', ['$scope', '$http', function( $scope, $http) {
 	
+	$scope.ranking = [];
+
+	$scope.outcome = '';
+	$scope.teamIds = [];
+
+	//method to read jsons coming from server
+	$scope.getJsonFromUrl = function (url, successCallback, errorCallback) {
+		$http.get(url).success(successCallback).error(errorCallback);
+	};
 
 	//Pulseball init
 	$scope.pulseballInit = function (rankingsJson) {
-
-		console.log(rankingsJson);
-
-		var successCallback = $scope.pulseballInitSuccess;
-		var errorCallback = $scope.pulseballInitError;
-
-		$http.get(rankingsJson).success(successCallback).error(errorCallback);
+		$scope.getJsonFromUrl( rankingsJson,  $scope.pulseballInitSuccess, $scope.pulseballInitError);
 
 	};
 
 	$scope.pulseballInitSuccess = function (response) {
-		console.log("data: ", response);
 		$scope.ranking = response;
 	};
 
 	$scope.pulseballInitError = function (response) {
-		console.log("error loading ", rankingsJson);
+		console.log("error loading init");
+	};
+
+
+	//get new match from server
+	$scope.addMatch = function () {
+		$scope.getJsonFromUrl( $scope.newMatch,  $scope.addMatchSuccess, $scope.addMatchError);
+	};
+
+
+	$scope.addMatchSuccess = function (response) {
+
+		console.log(response);
+
+		$scope.outcome = response.outcome;
+
+		if($scope.outcome != 'N') {
+
+			angular.forEach(response.teams, function(value, key) {
+			  	this.push(value.id);
+			}, $scope.teamIds);
+
+			$scope.calculateRatingDifference();
+		}
+
+	};
+
+	$scope.addMatchError = function (response) {
+		console.log("error loading match");
+	};
+
+	$scope.calculateRatingDifference = function () {
+
+		var actualPoints = [];
+		for (var i=0; i<$scope.teamIds.length; i++) {
+			actualPoints[i] = $scope.getActualPoints($scope.teamIds[i]);
+		}
+
+		var difference = (actualPoints[0] + 3) - actualPoints[1];
+		//round to two decimals
+		difference = difference.toFixed(2);
+
+		if (difference > 10) difference = 10;
+		if (difference < -10) difference = -10;
+
+		console.log(difference);
+
+		
+		
+
+
+	};
+
+	$scope.getActualPoints = function(id) {
+
+		var points = 0;
+
+		angular.forEach($scope.ranking, function(value, key) {
+			if(value.team.id == id) {
+				points = value.pts;
+			}	
+		});
+		return points;
 	};
 
 
@@ -31,6 +95,24 @@ pulseballApp.controller('pulseballAppCtrl', ['$scope', '$http', function( $scope
 
 
 
+		/*
+
+		//home wins
+		if (outcome == 'A') {
+
+		}
+
+		//away wins
+		if (outcome == 'B') {
+
+		}
+
+		//draw
+		if (outcome == 'D') {
+
+		}
+
+		*/
 
 
 
